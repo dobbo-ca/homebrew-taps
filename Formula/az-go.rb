90840261a2edc49d90c99885f8f9444568d76f96
Chris Dobbyn <8264498+cdobbyn@users.noreply.github.com>
2025-11-12 13:38:46 -0800

    feat: add az-go formula with azure-cli conflict detection

---
+++ Formula/az-go.rb
@@ -0,0 +1,39 @@
+class AzGo < Formula
+  desc "Azure CLI alternative written in Go"
+  homepage "https://github.com/cdobbyn/azure-go-cli"
+  version "0.1.0"
+  license "MIT"
+
+  # Conflict with official Azure CLI
+  conflicts_with "azure-cli", because: "both install 'az' binary"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/cdobbyn/azure-go-cli/releases/download/0.1.0/az-go-0.1.0-darwin-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/cdobbyn/azure-go-cli/releases/download/0.1.0/az-go-0.1.0-darwin-amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/cdobbyn/azure-go-cli/releases/download/0.1.0/az-go-0.1.0-linux-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/cdobbyn/azure-go-cli/releases/download/0.1.0/az-go-0.1.0-linux-amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "az"
+  end
+
+  test do
+    system "#{bin}/az", "--version"
+  end
+end
